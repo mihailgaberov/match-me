0e4ddf5a8b96630bf6c98430b1d27b6774e82dac
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-17 12:43:43 +0100

    :construction: Adding navigation

---
+++ app/components/navigation/TopNav.tsx
@@ -0,0 +1,9 @@
+import { Button, Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
+import Link from "next/link";
+import React from "react";
+import { GiSelfLove } from "react-icons/gi";
+
+
+export default async function TopNav() {
+    
+}
