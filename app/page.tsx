cd9b4dda6f1e38e3a5226bfeabd8d9a52c90ec35
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-17 11:56:55 +0100

    :spakles: add routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@nextui-org/react";
 import Image from "next/image";
+import Link from "next/link";
+import { GoSmiley } from "react-icons/go";
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">hello</h1>
+      <Button
+        as={Link}
+        href="/members"
+        variant="bordered"
+        color="danger"
+        startContent={<GoSmiley />}
+      >
+        Click me
+      </Button>
+    </div>
+  );
 }
